dffefdb9aef0fcf01c42513641060463c3794ae8
kanishak garg <kanishakgarg8778@gmail.com>
2021-09-21 23:30:53 +0530

    add state to CartItem component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
+
     render(){
+        const {price, title, qty} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
@@ -9,12 +22,15 @@ class CartItem extends React.Component{
                 </div>
 
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Price</div>
-                    <div style={{color: '#777'}} >Rs 999</div>
-                    <div style={{color: '#777'}} >Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color: '#777'}} >Rs {price}</div>
+                    <div style={{color: '#777'}} >Qty: {qty} </div>
                     
                     <div className="cart-item-actions">
                         {/* buttons*/}
+                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" />
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
+                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" />
                     </div>
                 </div>
             </div>
