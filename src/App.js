1a22f0f342aa3207ff6cb65a2e9e2f684ed56ddd
kanishak garg <kanishakgarg8778@gmail.com>
2021-09-23 22:56:03 +0530

    add images to the products

--- src/App.js
+++ src/App.js
@@ -11,21 +11,21 @@ class App extends React.Component {
                 price: 99,
                 title: 'watch',
                 qty: 1,
-                img: '',
+                img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=388&q=80',
                 id: 1
             },
             {
                 price: 999,
                 title: 'Mobile Phone',
                 qty: 1,
-                img: '',
+                img: 'https://images.unsplash.com/photo-1580910051074-3eb694886505?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=465&q=80',
                 id: 2
             },
             {
                 price: 9999,
                 title: 'laptop',
                 qty: 4,
-                img: '',
+                img: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80',
                 id: 3
             },
         ]
